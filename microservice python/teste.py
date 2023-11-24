520a7d2717947644964da1a00a623dba9f622f99
Caussz <cczcaua@gmail.com>
2023-11-24 14:01:01 -0300

    (init) transcrição código de js para python

---
+++ microservice python/teste.py
@@ -0,0 +1,34 @@
+from openai import OpenAI
+
+client = OpenAI(api_key="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")
+import os
+
+# Load API key from environment variable
+
+
+def qea(prompt):
+    completion = client.chat.completions.create(model="gpt-3.5-turbo",
+    messages=[{"role": "user", "content": prompt}],
+    temperature=0.7)
+    print(completion)
+    return completion
+
+def translate(prompt, from_lang, to_lang):
+    completion = client.chat.completions.create(model="gpt-3.5-turbo",
+    messages=[{"role": "user", "content": f"Translate {prompt} from {from_lang} to {to_lang}"}],
+    temperature=0.7)
+    return completion
+
+def summarize(prompt):
+    completion = client.chat.completions.create(model="gpt-3.5-turbo",
+    messages=[{"role": "user", "content": f"Summarize {prompt}"}],
+    temperature=0.7)
+    return completion
+
+def get_keywords(prompt):
+    completion = client.chat.completions.create(model="gpt-3.5-turbo",
+    messages=[{"role": "user", "content": f"Get 7 keywords from {prompt}"}],
+    temperature=0.7)
+    return completion
+
+qea('oi tudo bem?')
